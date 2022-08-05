b10fd1cb8be55cad6d1dfcdd8c16d190bbf25437
nicolasarths <nicolasarths@hotmail.com>
2022-08-05 14:50:27 -0300

    Database now automatically indexes added items, still testing it tho

--- model/Database.js
+++ model/Database.js
@@ -1,34 +1,57 @@
 //Type ``npm run test Database`` to run tests
 
-export default class Database {
+class Database {
     constructor(){
         this.data = []
+        this.items = []
+    }
+
+    #store(data){ 
+        this.data.push({
+            index: this.data.length,
+            item: data
+        })
+        this.items.push(data)
     }
 
     store(data){
         if (Array.isArray(data)){
-            data.forEach(item => this.data.push(item))
+            data.forEach(item => this.#store(item))
         } else {
-            this.data.push(data)
+            this.#store(data)
         }
         return true;
     }
 
     retrieve(query){
-        if (arguments[0] === undefined){
-            return this.data
+        if (query === undefined){
+            return this.items
         }
-        const index = this.data.indexOf(query)
-        const data = this.data[index]
+        const index = this.items.indexOf(query)
+        const data = this.items[index]
         return data
     }
 
+    #findItem(query){
+        return this.data.find(item => item.item === query)
+    }
+
+    delete(){
+        this.dropAll()
+    }
+
     update(data, update){
-        this.data[0] = update
+       /*  const {index} = this.#findItem(data)
+        console.log(this.data[index])
+        this.data[index] = 
+        console.log(this.data[index]) */
+        this.store(update)
     }
 
     dropAll(){
         this.data = []
+        this.items = []
     }
-
 }
+
+export default new Database()
