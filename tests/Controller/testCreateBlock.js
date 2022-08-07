917dd8f810f5195db65b7e9c3404c8f5c73745a4
nicolasarths <nicolasarths@hotmail.com>
2022-08-07 19:06:10 -0300

    files moved to controllerTest or databaseTest

--- tests/Controller/testCreateBlock.js
+++
@@ -1,7 +0,0 @@
-import Controller from "../../controller";
-
-const request = ({})
-
-export default function () {
-    expect(Controller.createBlock(request)).toBeCalled()
-}
--- tests/Database/testDrop.js
+++
@@ -1,9 +0,0 @@
-import { Database } from "../../model"
-import mock from '../testHelpers/mock'
-
-export default it('.dropAll() erases all data and resets idCounter', () => {  
-    mock.randomValues.forEach(value => Database.store(value))
-    expect(Database.retrieve()).not.toBe([])
-    Database.dropAll()
-    expect(Database.retrieve()).toEqual([])
-})
