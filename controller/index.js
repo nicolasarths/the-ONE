6d4309cf9e34f5b1deabb3770fd9110380a2018b
nicolasarths <nicolasarths@hotmail.com>
2022-08-07 19:01:56 -0300

    Factory currently stores requested blocks in the database

--- controller/index.js
+++ controller/index.js
@@ -1,8 +1,13 @@
 import {Database, Block} from '../model'
+import RequestHandler from './RequestHandler'
 
 export default class Controller {
     static createDefaultBlock () {
         return Database.store(new Block())
+    }
+
+    static requestHandler(req){
+        return RequestHandler.respondTo(req)
     }
     
     static loadBlocks () {
