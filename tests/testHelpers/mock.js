600f71d9ad34fbf622624ccd80946f70574fbbb4
nicolasarths <nicolasarths@hotmail.com>
2022-08-07 03:01:47 -0300

    Some mocking functions have been created to help testing, such as the creation of an array of random types of data, if you change the database model you have to change the mock once, and then every test is supposed to keep working still

---
+++ tests/testHelpers/mock.js
@@ -0,0 +1,35 @@
+import { Database, Block } from "../../model"
+import Controller from "../../controller"
+
+function block (id,item){
+    return {
+        id,
+        item
+    }
+}
+
+const randomValues = [
+    new Block(),
+    1,
+    1.02,
+    [1, 1.9],
+    'a1@$1.0+_||%%z\'\\\\',
+    function (a, b) {
+        return
+    },
+    ['a', 'b', 'c'],
+    'str',
+    { a: 1, b: 2, c:3 }
+]
+
+function mockBlocksOfRandomValues() {
+    const mock = []
+    randomValues.forEach((value, i) => mock.push(block(i+1, value)))
+    return mock
+}
+
+export default {
+    block,
+    randomValues,
+    mockBlocksOfRandomValues
+}
