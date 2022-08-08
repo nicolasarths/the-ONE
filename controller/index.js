0074cab2a7c025e68ceed46a3aa230928127c8f9
nicolasarths <nicolasarths@hotmail.com>
2022-08-08 20:04:52 -0300

    deleted redundant functions from controllers

--- controller/index.js
+++ controller/index.js
@@ -1,20 +1,7 @@
 import {Database, Block} from '../model'
+import responses from './responses'
 import RequestHandler from './RequestHandler'
 
 export default class Controller {
-    static createDefaultBlock () {
-        return Database.store(new Block())
-    }
-
-    static requestHandler(req){
-        return RequestHandler.respondTo(req)
-    }
-    
-    static loadBlocks () {
-        return Database.retrieve()
-    }
-
-    static editBlock () {
-        return true
-    }
+    static handle(req) { return RequestHandler(req) }
 }
