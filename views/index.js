4033f90ae605d7a35d892b222f6ef61e26908e45
nicolasarths <nicolasarths@hotmail.com>
2022-08-08 20:07:53 -0300

    proto views being developed

--- views/index.js
+++ views/index.js
@@ -1,5 +1,18 @@
-export default class BlockView {
-    constructor(){
+import Factory from "../controller/Factory"
+import responses from "../controller/responses"
+import { Database } from "../model"
 
+export default class BlockView {
+    constructor(block){
+        this.title = block.title
     }
+
+    static constructViews(){
+        const grid = Factory.constructGrid()
+        if (grid.length == 3) {
+
+            return responses.success
+        }
+    }
+
 }
