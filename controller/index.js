e7e4ac6683de984ab3f1d1bb78372c6ca8327d5d
nicolasarths <nicolasarths@hotmail.com>
2022-08-08 22:45:39 -0300

    removed nonused imports

--- controller/index.js
+++ controller/index.js
@@ -1,7 +1,6 @@
-import {Database, Block} from '../model'
-import responses from './responses'
 import RequestHandler from './RequestHandler'
 
 export default class Controller {
     static handle(req) { return RequestHandler(req) }
+
 }
