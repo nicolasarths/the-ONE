19f9d50f1a71d606abbacf931f5a54aeab4e91bb
nicolasarths <nicolasarths@hotmail.com>
2022-08-08 22:43:03 -0300

    added export for Block model

--- tests/controllerTests/requestHandlerTests/index.js
+++ tests/controllerTests/requestHandlerTests/index.js
@@ -2,11 +2,13 @@ import testCreateRequest from "./testCreateRequest"
 import testReadRequest from "./testReadRequest"
 
 import Controller from "../../../controller";
+import { Block } from '../../../model';
 import responses from "../../../controller/responses";
 import mockCreateRequest from '../../testHelpers/mockCreateRequest'
-export { Controller, responses, mockCreateRequest }
+export { Controller, responses, mockCreateRequest, Block }
 
 export default describe('Request Handler', () => {
     testCreateRequest
     testReadRequest
+    afterReadDashboardRequest
 })
